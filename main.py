1e51497c71ac4afe2ede35152b14f5b4c7678855
KosarevMaxim <kosarev.maxim99@gmail.com>
2023-04-23 17:34:25 +0500

    DataSet v3

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import dvc.api
 import pandas as pd
 
-data_path = dvc.api.get_url('Data_sets/dataset1.csv', remote="data_remote2")
+data_path = dvc.api.get_url('Data_sets/dataset3.csv', remote="data_remote2")
 df = pd.read_csv(data_path.replace("root/", ""))
 print(df)
