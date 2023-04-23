32872002751cfcf0cb09f1608bd5634911b7b1ab
KosarevMaxim <kosarev.maxim99@gmail.com>
2023-04-23 16:23:07 +0500

    Load Data set

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import dvc.api
 import pandas as pd
 
-data_path = dvc.api.get_url('Data_sets/dataset1.csv')
+data_path = dvc.api.get_url('Data_sets/dataset1.csv', remote="data_remote")
 df = pd.read_csv(data_path)
 print(df)
